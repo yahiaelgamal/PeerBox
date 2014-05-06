a4263bd7fefcce8a0ae219b17d8583fa7ad23abd
Saftophobia <asafty2003@gmail.com>
2014-05-06 19:21:48 +0200

    added naive constructor

--- PeerBox/src/tomPeer/SamplePeer.java
+++ PeerBox/src/tomPeer/SamplePeer.java
@@ -1,14 +1,30 @@
 package tomPeer;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
+import net.tomp2p.connection.Bindings;
 import net.tomp2p.p2p.Peer;
+import net.tomp2p.p2p.PeerMaker;
+import net.tomp2p.peers.Number160;
 
 public class SamplePeer {
 	private Peer peer;
 	private String peerID;
 	private String shareFolderPath;
 
-	public SamplePeer() {
-
+	
+	public SamplePeer(InetAddress IPAddress, String bootstrapIP, String shareFolderPath) throws IOException {
+		this.setShareFolderPath(shareFolderPath);
+		
+		//port and peerID need to set somehow
+		peer = new PeerMaker(Number160.createHash(this.getPeerID())).setPorts(123).setBindings(new Bindings(IPAddress)).makeAndListen();
+		
+		if(bootstrapIP != null && !bootstrapIP.replaceAll("\\s+","").isEmpty()){
+			//bootstrap to well-known IP
+		}
+		//add myself to DHT
+		//open peer for connections, somehow
 	}
 
 	public void checkForUpdates() {

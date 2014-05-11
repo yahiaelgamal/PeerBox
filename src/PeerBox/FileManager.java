901c6431c123b132460ae3a61a12518a2f8b807b
DevYah <yahiaelgamal@gmail.com>
2014-05-11 21:26:19 +0200

    Change tabs to spaces

--- src/PeerBox/FileManager.java
+++ src/PeerBox/FileManager.java
@@ -16,54 +16,54 @@ import org.json.simple.JSONValue;
 import de.uniba.wiai.lspi.chord.console.command.entry.Key;
 
 public class FileManager {
-	public String workingDir;
-	public final  int PIECE_SIZE = 512 * 1024;
-	
-	public FileManager(String folderName) {
-    	String projDir = System.getProperty("user.dir");
- 
-    	this.workingDir = projDir + File.separator + "peersData" + File.separator + folderName;
+    public String workingDir;
+    public final  int PIECE_SIZE = 512 * 1024;
 
-    	System.out.println("working idr : " + workingDir);
-    	File dir = new File(workingDir);
-    	if(!dir.exists())
-    		dir.mkdir();
-	}
-	
-	public String buildFullPath(String relativePath) {
-		return workingDir + File.separator + relativePath;
-	}
-	
-	public LinkedList<String> splitFiles(String relativePathToFile) throws IOException {
-		File f = new File(buildFullPath(relativePathToFile));
-		LinkedList <String> splitted = FileSplitAndMerge.splitFile(f);
-		return splitted;
-	}
-	
-	public boolean writeToFile(String relativePath, byte[] content) {
-		try {
-			String fullPath = buildFullPath(relativePath);
-			File file = new File(fullPath);
-			FileOutputStream fos = new FileOutputStream(file);
-			fos.write(content);
-			fos.flush();
-			fos.close();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	
-	public static String convertMapToJSONString(Map map) {
-		String jsonString = JSONObject.toJSONString(map);
-		return jsonString;
-	}
-	
-//	@SuppressWarnings("unchecked")
-	public static void main(String[] args) {
-		Key key = new Key("key 1");
-	}
-	
+    public FileManager(String folderName) {
+        String projDir = System.getProperty("user.dir");
+
+        this.workingDir = projDir + File.separator + "peersData" + File.separator + folderName;
+
+        System.out.println("working idr : " + workingDir);
+        File dir = new File(workingDir);
+        if(!dir.exists())
+            dir.mkdir();
+    }
+
+    public String buildFullPath(String relativePath) {
+        return workingDir + File.separator + relativePath;
+    }
+
+    public LinkedList<String> splitFiles(String relativePathToFile) throws IOException {
+        File f = new File(buildFullPath(relativePathToFile));
+        LinkedList <String> splitted = FileSplitAndMerge.splitFile(f);
+        return splitted;
+    }
+
+    public boolean writeToFile(String relativePath, byte[] content) {
+        try {
+            String fullPath = buildFullPath(relativePath);
+            File file = new File(fullPath);
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(content);
+            fos.flush();
+            fos.close();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static String convertMapToJSONString(Map map) {
+        String jsonString = JSONObject.toJSONString(map);
+        return jsonString;
+    }
+
+//  @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        Key key = new Key("key 1");
+    }
+
 }

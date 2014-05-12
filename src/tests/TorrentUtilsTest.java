66c5459a1a945c21836f26af007766c33e5181f8
DevYah <yahiaelgamal@gmail.com>
2014-05-12 02:24:22 +0200

    Change tabs in tests file

--- src/tests/TorrentUtilsTest.java
+++ src/tests/TorrentUtilsTest.java
@@ -13,34 +13,34 @@ import PeerBox.TorrentUtils;
 
 public class TorrentUtilsTest {
 
-	public static String[][] getTestConfig(){
-		String[][] info = { {"key1", "EncKey1", "init1"},
-							{"key2", "EncKey2", "init2"},
-							{"key3", "EncKey3", "init3"} };
-		return info;
-	}
-	
-	public static String getTestConfigString() {
-		return "[[\"key1\",\"EncKey1\",\"init1\"],[\"key2\",\"EncKey2\",\"init2\"],[\"key3\",\"EncKey3\",\"init3\"]]";
-	}
-	public static boolean TestBasicConversion() {
-		String[][] info = getTestConfig();
-		
-		JSONArray jsonArr = TorrentUtils.convertTorrentContent(info);
-		return (jsonArr.toJSONString().equals(getTestConfigString()));
-	}
-	
-	public static boolean testWriteTorrentConfig() throws IOException{
-		FileManager fm = new FileManager("tests" + File.separator + "peer_test");
-		TorrentUtils.writeTorrentConfigToFile(fm, "test_torrent.json", getTestConfig());
-		byte[] bytes = FileManager.readFile(fm.buildFullPath("test_torrent.json"));
-		String s = new String(bytes);
-		return s.equals(getTestConfigString());
-	}
+    public static String[][] getTestConfig(){
+        String[][] info = { {"key1", "EncKey1", "init1"},
+                            {"key2", "EncKey2", "init2"},
+                            {"key3", "EncKey3", "init3"} };
+        return info;
+    }
+
+    public static String getTestConfigString() {
+        return "[[\"key1\",\"EncKey1\",\"init1\"],[\"key2\",\"EncKey2\",\"init2\"],[\"key3\",\"EncKey3\",\"init3\"]]";
+    }
+    public static boolean TestBasicConversion() {
+        String[][] info = getTestConfig();
+
+        JSONArray jsonArr = TorrentUtils.convertTorrentContent(info);
+        return (jsonArr.toJSONString().equals(getTestConfigString()));
+    }
+
+    public static boolean testWriteTorrentConfig() throws IOException{
+        FileManager fm = new FileManager("tests" + File.separator + "peer_test");
+        TorrentUtils.writeTorrentConfigToFile(fm, "test_torrent.json", getTestConfig());
+        byte[] bytes = FileManager.readFile(fm.buildFullPath("test_torrent.json"));
+        String s = new String(bytes);
+        return s.equals(getTestConfigString());
+    }
 
     public static boolean testTorrentConfig() throws ParseException, IOException, org.json.simple.parser.ParseException {
-		FileManager fm = new FileManager("tests" + File.separator + "peer_test");
-		TorrentUtils.writeTorrentConfigToFile(fm, "test_torrent.json", getTestConfig());
+        FileManager fm = new FileManager("tests" + File.separator + "peer_test");
+        TorrentUtils.writeTorrentConfigToFile(fm, "test_torrent.json", getTestConfig());
 
         TorrentConfig tc = new TorrentConfig(fm.buildFullPath("test_torrent.json"));
         return tc.getAllKeys().get(0).equals("key1") &&
@@ -48,9 +48,9 @@ public class TorrentUtilsTest {
                tc.getAllKeys().get(2).equals("key3");
     }
 
-	public static void main(String[] args) throws Exception{
-		System.out.println(TestBasicConversion());
-		System.out.println(testWriteTorrentConfig());
-		System.out.println(testTorrentConfig());
-	}
+    public static void main(String[] args) throws Exception{
+        System.out.println(TestBasicConversion());
+        System.out.println(testWriteTorrentConfig());
+        System.out.println(testTorrentConfig());
+    }
 }
